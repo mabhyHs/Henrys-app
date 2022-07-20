6e56f8f203c2e987d0a855db2796b59ed321c758
Nicolas Pavon <nicosubealanube@gmail.com>
2022-07-20 19:11:05 -0300

    hotfix EsLint

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './Redux/store/store';
+import store from './Redux/store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
