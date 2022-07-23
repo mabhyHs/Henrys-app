7cdead97d2ff693a4fe2a80db4c3734e0053d57f
mabhyHs <m4huens@gmail.com>
2022-07-23 18:58:21 -0300

    add button back

--- client/src/Components/ProductDetail/ProductDetail.jsx
+++ client/src/Components/ProductDetail/ProductDetail.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 import imgProduct from '../../Assets/Images/Hamburguesas/hamburguesa-clasica.png';
 
 import './ProductDetail.css';
@@ -16,6 +18,9 @@ function ProductDetail() {
         />
         <h2>Descripción:</h2>
         <p className="productDetail__text">Tomate</p>
+        <Button as={Link} to="/menu" className="mt-3 mb-5">
+          Volver al Menú
+        </Button>
       </Container>
     </div>
   );
