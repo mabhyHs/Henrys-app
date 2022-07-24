e2bdece640ad0352c4b3072a6b196ad10475da58
mabhyHs <m4huens@gmail.com>
2022-07-24 17:34:08 -0300

    se agrega estilo para el grid de cards en Menu

--- client/src/Components/ProductsContainerMenu/ProductsContainerMenu.jsx
+++ client/src/Components/ProductsContainerMenu/ProductsContainerMenu.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import CardProductMenu from '../CardProductMenu/CardProductMenu';
+import Container from 'react-bootstrap/Container';
+
+import './ProductsContainerMenu.css';
 
 function ProductsContainerMenu() {
   return (
     <div>
-      <CardProductMenu />
+      <Container className="products__container__menu mt-3">
+        <CardProductMenu />
+      </Container>
     </div>
   );
 }
