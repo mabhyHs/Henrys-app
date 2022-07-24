15e25a024e2055cf10090051907bdb1037eeabf6
siprem10 <siprem10@gmail.com>
2022-07-24 04:50:17 -0300

    fix-eslint

--- client/src/Components/AddBurger/AddBurger.jsx
+++ client/src/Components/AddBurger/AddBurger.jsx
@@ -1,24 +1,17 @@
-/* eslint-disable func-names */
-/* eslint-disable no-console */
-/* eslint-disable react/button-has-type */
 import React, {useState} from "react";
 import {connect} from 'react-redux'
 
 
-// eslint-disable-next-line func-names
 const mapStateToProps = function(state){
     return {ingredients: state.ingredients}
 }
 const modificarIngredientes = function(id, ingredientes, cb){
-    // eslint-disable-next-line no-param-reassign
     cb(ingredientes = ingredientes.filter((i) => i.id !== id))
 }
-// eslint-disable-next-line func-names
+
 const añadirIngredientes = function(e, ingredientes, cb, igrGlobal){
     const ingredienteSelect = igrGlobal.find((i) => i.name === e.target.value)
-    // eslint-disable-next-line prefer-const
     let prueba = []
-    // eslint-disable-next-line no-restricted-syntax
     for(const ele of ingredientes){
         if(ele.name === ingredienteSelect.name){
             prueba.push(ele)
@@ -30,38 +23,29 @@ const añadirIngredientes = function(e, ingredientes, cb, igrGlobal){
         prueba = []
     }else{
         ingredienteSelect.cantidad = 1
-        // eslint-disable-next-line no-param-reassign
         cb(ingredientes = [...ingredientes, ingredienteSelect])
     }
 }
-// eslint-disable-next-line func-names
+
 const cambiarCantidad = function(e, id, ingredientes, cb){
     const ingredienteSelect = ingredientes.find((i) => i.id === id)
     ingredienteSelect.cantidad = e.target.value
-    const copiaIngredientes = [...ingredientes]
-    // eslint-disable-next-line no-const-assign
+    let copiaIngredientes = [...ingredientes]
     copiaIngredientes = copiaIngredientes.filter((i) => i.id !== id)
-    // eslint-disable-next-line no-const-assign
     copiaIngredientes = [...copiaIngredientes , ingredienteSelect]
     
-    // eslint-disable-next-line no-param-reassign
     cb(ingredientes = [...copiaIngredientes])
     
     
 }
 const crearBurguer = function(precio, cb, ingredientes, setIngredientsAdd){
     let sumaTotal = 0
-    // eslint-disable-next-line no-restricted-syntax
     for(const ele of ingredientes){
-        // eslint-disable-next-line prefer-const
         let {cantidad} = ele
         sumaTotal += (ele.price * cantidad) 
     }
-    // eslint-disable-next-line no-param-reassign
     cb(precio = sumaTotal)
-    // eslint-disable-next-line no-param-reassign
     setIngredientsAdd(ingredientes = [{id: 10000, name:'pan', price: 0.5, cantidad: 2}])
-    // eslint-disable-next-line no-alert
     alert(`su hamburguesa tiene un precio de: ${  sumaTotal}`)
 }   
 
