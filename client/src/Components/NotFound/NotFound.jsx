1637c8ac7b845d5161d604151973ca0a0d9a7007
mabhyHs <m4huens@gmail.com>
2022-07-23 23:27:45 -0300

    Hotfix(Fixing Styles)

--- client/src/Components/NotFound/NotFound.jsx
+++ client/src/Components/NotFound/NotFound.jsx
@@ -8,11 +8,11 @@ export default function NotFound() {
   return (
     <div className={styles.page_404}>
       <div className={styles.four_zero_four_bg}>
-        <h1 className={styles.h1}>404</h1>
+        <h1 className={styles.page__404__h1}>404</h1>
       </div>
 
       <div className={styles.contant_box_404}>
-        <h3 className={styles.h3}>Parece que te perdiste...</h3>
+        <h3 className={styles.page__404__h3}>Parece que te perdiste...</h3>
         <p>La pagína solicitada no se encuentra ó no está disponible </p>
         <Button className={styles.btn} as={Link} to="/">
           Volver
