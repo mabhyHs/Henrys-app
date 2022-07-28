4911a352468a00ba2b37e1b648429a9eb02c1ef3
unknown <peluffojuancruz@gmail.com>
2022-07-28 15:11:30 -0300

    delete, update, restore fries

--- api/src/controllers/fries.controllers.js
+++ api/src/controllers/fries.controllers.js
@@ -10,6 +10,41 @@ async function create(req, res, next) {
   }
 }
 
+async function destroy(req, res, next) {
+  try {
+    const { id } = req.params;
+    const fries = await friesRepository.destroy(id);
+    fries ? res.status(200).json("succes") : res.status(400).json("error");
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function update(req, res, next) {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const fries = await friesRepository.update(id, data);
+    res.status(201).json(fries);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function restore(req, res, next) {
+  try {
+    const { id } = req.params;
+
+    const fries = await friesRepository.restore(id);
+    fries ? res.status(200).json("succes") : res.status(400).json("error");
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   create,
+  destroy,
+  update,
+  restore,
 };
