cab1a4e29f76cffe816d2a4ff526a18d0261c014
maurorodriguez <mauroalejo2002@hotmail.com>
2022-07-29 16:01:14 -0300

    feat(src/): Users delete, restore and update

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { create } = require("../controllers/users.controllers");
+const {
+  create,
+  destroy,
+  restore,
+  update,
+} = require("../controllers/users.controllers");
 
 const router = express.Router();
 
 router.post("/", create);
+router.delete("/:id", destroy);
+router.post("/:id", restore);
+router.put("/", update);
 
 module.exports = router;
