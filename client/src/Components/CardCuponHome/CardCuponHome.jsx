bf65d0ac0c0a7cb28d707a3f99f002bcd4cb71da
mabhyHs <m4huens@gmail.com>
2022-07-29 16:49:55 -0300

    fix(UI-App)

--- client/src/Components/CardCuponHome/CardCuponHome.jsx
+++ client/src/Components/CardCuponHome/CardCuponHome.jsx
@@ -9,30 +9,52 @@ function CardCupponHome() {
   return (
     <section className="sectionCupponsHome">
       <Card className="cupponCardHome">
-        <Card.Img variant="top" src={hambCupon} className="card__img__top" />
+        <Card.Img
+          variant="top"
+          src={hambCupon}
+          className="card__img__top img-fluid"
+        />
         <Card.Body className="cupponCardHome__cardBody">
           <Card.Title>
             <h2>CheckPoint Day</h2>
           </Card.Title>
           <Card.Text>
-            <h2>20%off</h2>
+            <p>20%off</p>
           </Card.Text>
-          <Button variant="primary" className="cupponCardHome__cupponButton">
+          <Button
+            onClick={() => {
+              navigator.clipboard.writeText('RTTAS87');
+              alert('Código copiado al portapapeles con éxito');
+            }}
+            variant="primary"
+            className="cupponCardHome__cupponButton"
+          >
             RTTAS87
           </Button>
         </Card.Body>
       </Card>
 
       <Card className="cupponCardHome">
-        <Card.Img variant="top" src={friesCupon} className="card__img__top" />
+        <Card.Img
+          variant="top"
+          src={friesCupon}
+          className="card__img__top img-fluid"
+        />
         <Card.Body className="cupponCardHome__cardBody">
           <Card.Title>
             <h2>Dia de fritas</h2>
           </Card.Title>
           <Card.Text>
-            <h2>15%off</h2>
+            <p>15%off</p>
           </Card.Text>
-          <Button variant="primary" className="cupponCardHome__cupponButton">
+          <Button
+            onClick={() => {
+              navigator.clipboard.writeText('XLXR567');
+              alert('Código copiado al portapapeles con éxito');
+            }}
+            variant="primary"
+            className="cupponCardHome__cupponButton"
+          >
             XLXR567
           </Button>
         </Card.Body>
