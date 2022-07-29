933470aad7edc8117ce6be095362a4f21fe4b80f
siprem10 <siprem10@gmail.com>
2022-07-29 20:17:25 -0300

    src

--- api/src/models/Newsletter.js
+++ api/src/models/Newsletter.js
@@ -3,7 +3,7 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Newsletter extends Model {
   }
-  User.init(
+  Newsletter.init(
     {
       id: {
         type: DataTypes.UUID,
