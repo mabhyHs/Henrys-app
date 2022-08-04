b823d466ef37a11777ae419bb61c16588a5efcc3
siprem10 <siprem10@gmail.com>
2022-08-04 16:47:08 -0300

    activate(mejorado)

--- client/src/Components/User/UserActivateAccount/UserActivateAccount.jsx
+++ client/src/Components/User/UserActivateAccount/UserActivateAccount.jsx
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/esm/Container';
 import Button from 'react-bootstrap/esm/Button';
 import ActivateImg from '../../../Assets/Images/combos/combo2-dobles.png';
 import { Link, useParams } from 'react-router-dom';
-import { FcHighPriority, FcOk } from 'react-icons/fc';
+import { FcAdvertising, FcHighPriority, FcOk } from 'react-icons/fc';
 
 import './UserActivateAccount.css';
 import axios from 'axios';
@@ -11,7 +11,7 @@ import Swal from 'sweetalert2';
 
 function UserActivateAccount() {
   const [mount, setMount] = useState(false);
-  const [isSuccess, setSucess] = useState(false);
+  const [isSuccess, setSucess] = useState(-1);
 
   const { id } = useParams();
   useEffect(() => {
@@ -21,16 +21,24 @@ function UserActivateAccount() {
       const fetchData = async (id) => {
         try {
           await axios.put(`/activateAccount/${id}`);
-          setSucess(true);
+          setSucess(1);
         } catch (error) {
+            let imgUrl = "https://res.cloudinary.com/henrysburgers/image/upload/v1659301854/error-henrys_zoxhtl.png";
+            const msg = error.response.data.error;
+            if(typeof(msg) === "string" && msg === "La cuenta ya fue activada!"){
+                imgUrl = "https://res.cloudinary.com/henrysburgers/image/upload/v1659640839/advert_aotsaj.png";
+                setSucess(0);
+            }else{
+                setSucess(-1);
+            }
+
             Swal.fire({
                 customClass: {
                   confirmButton: 'confirmBtnSwal',
                 },
                 title: 'Opss...',
-                text: typeof(error.response.data.error) !== "string" ? "Error al activar la cuenta!" : error.response.data.error,
-                imageUrl:
-                  'https://res.cloudinary.com/henrysburgers/image/upload/v1659301854/error-henrys_zoxhtl.png',
+                text: typeof(msg) !== "string" ? "Error al activar la cuenta!" : msg,
+                imageUrl: imgUrl,
                 imageWidth: 170,
                 imageHeight: 170,
                 imageAlt: 'Logo henrys',
@@ -46,15 +54,18 @@ function UserActivateAccount() {
       <Container>
         <div className="userActivate__container">
           <div>
-            <h1>{isSuccess ? "¡Bienvenido!" : ""}</h1>
-            <h2 className="userActivate__subtittle">
-            {isSuccess ? <FcOk /> : <FcHighPriority /> }
-              {isSuccess ? "Cuenta verificada con éxito" : " Error al activar la cuenta!"}
+            <h1>{isSuccess !== 0 ? "¡Bienvenido!" : ""}</h1>
+            <h2 className="userActivate__subtittle"> 
+              {isSuccess === 1 && <FcOk />}
+              {isSuccess === 0 && <FcAdvertising />}
+              {isSuccess === -1 && <FcHighPriority />}               
+              {isSuccess === 1 && " Cuenta activada con éxito"}
+              {isSuccess === 0 && " La cuenta ya fue activada!"}
+              {isSuccess === -1 && " Error al activar la cuenta!"}
             </h2>
-            <p>{isSuccess ? "Empezá a disfrutar de las más deliciosas hamburguesas" : ""}</p>
-            
+            <p>{isSuccess === 1 ? "Empezá a disfrutar de las más deliciosas hamburguesas" : ""}</p> 
             <Button as={Link} to="/">
-                {isSuccess ? "Iniciar la experiencia" : "Volver al menú"}
+                {isSuccess === 1 ? "Iniciar la experiencia" : "Volver al menú"}
             </Button>
           </div>
           <img
