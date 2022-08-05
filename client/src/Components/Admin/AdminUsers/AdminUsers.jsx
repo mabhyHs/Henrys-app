9d12d4d2fb8b1eddd82bab3794fc44ce1b3ef379
mabhyHs <m4huens@gmail.com>
2022-08-05 12:59:51 -0300

    Create(UI-modal editar rol de usuario)

--- client/src/Components/Admin/AdminUsers/AdminUsers.jsx
+++ client/src/Components/Admin/AdminUsers/AdminUsers.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 import {
   PersonCheckFill,
@@ -13,6 +15,10 @@ import {
 import './AdminUsers.css';
 
 function AdminUsers() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <Container>
       <div className="adminUsers__container">
@@ -46,9 +52,36 @@ function AdminUsers() {
               <td>aguslopez@gmail.com</td>
               <td>USUARIO</td>
               <td className="adminUser__td__btns">
-                <Button variant="outline-secondary" size="sm">
+                <Button
+                  variant="outline-secondary"
+                  size="sm"
+                  onClick={handleShow}
+                >
                   <PencilSquare />
                 </Button>
+
+                <Modal show={show} onHide={handleClose}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Modificar Rol</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <Form.Select
+                      aria-label="selectRol"
+                      defaultValue="Selecionar rol"
+                    >
+                      <option>Selecionar</option>
+                      <option value="admin">ADMIN</option>
+                      <option value="empleado">EMPLEADO</option>
+                      <option value="usuario">USUARIO</option>
+                    </Form.Select>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="primary" onClick={handleClose}>
+                      Confirmar
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+
                 <Button variant="outline-success" size="sm">
                   <PersonCheckFill />
                 </Button>
