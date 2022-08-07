bb22667b82bdc80f235402e13fe5507d6fd205f8
unknown <peluffojuancruz@gmail.com>
2022-08-07 11:38:23 -0300

    add nodemailer to createOrder

--- api/src/controllers/order.controllers.js
+++ api/src/controllers/order.controllers.js
@@ -1,8 +1,164 @@
+const axios = require("axios");
 const orderRepositories = require("../repositories/order.repositories");
 
 async function create(req, res, next) {
   try {
     const order = await orderRepositories.create(req.body.user.id, req.body);
+    const receipt = await mercadopagoRepository.getPaymentById(
+      req.body.purchaseId
+    );
+    console.log(receipt);
+
+    await transporter.sendMail({
+      from: '"Confirm account" <henrysBurger2022@gmail.com',
+      to: data.email,
+      subject: "Receipt",
+      html: `
+        <html lang="en-US">
+        <head>
+          <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+          <title>Henry's Burger</title>
+          <meta name="description" content="Henry's Burger" />
+          <style type="text/css">
+            a:hover {
+              text-decoration: underline !important;
+            }
+          </style>
+        </head>
+      
+        <body
+          marginheight="0"
+          topmargin="0"
+          marginwidth="0"
+          style="margin: 0px; background-color: #f2f3f8"
+          leftmargin="0"
+        >
+          <!-- 100% body table -->
+          <table
+            cellspacing="0"
+            border="0"
+            cellpadding="0"
+            width="100%"
+            bgcolor="#f2f3f8"
+            style="
+              @import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700);
+              font-family: 'Open Sans', sans-serif;
+            "
+          >
+            <tr>
+              <td>
+                <table
+                  style="background-color: #f2f3f8; max-width: 670px; margin: 0 auto"
+                  width="100%"
+                  border="0"
+                  align="center"
+                  cellpadding="0"
+                  cellspacing="0"
+                >
+                  <tr>
+                    <td style="height: 80px">&nbsp;</td>
+                  </tr>
+                  <tr>
+                    <td style="height: 20px">&nbsp;</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <table
+                        width="95%"
+                        border="0"
+                        align="center"
+                        cellpadding="0"
+                        cellspacing="0"
+                        style="
+                          max-width: 670px;
+                          background: #fff;
+                          border-radius: 3px;
+                          text-align: center;
+                          -webkit-box-shadow: 0 6px 18px 0 rgba(0, 0, 0, 0.06);
+                          -moz-box-shadow: 0 6px 18px 0 rgba(0, 0, 0, 0.06);
+                          box-shadow: 0 6px 18px 0 rgba(0, 0, 0, 0.06);
+                        "
+                      >
+                        <tr>
+                          <td style="height: 40px">&nbsp;</td>
+                        </tr>
+                        <tr>
+                          <td style="text-align: center">
+                            <a
+                              href="https://henrys-app.vercel.app/"
+                              title="logo"
+                              target="_blank"
+                            >
+                              <img
+                                width="60"
+                                src="https://i.postimg.cc/Y0T86N5w/logo-henrys300px.png"
+                                title="logo"
+                                alt="logo"
+                              />
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding: 0 35px">
+                            <h1
+                              style="
+                                color: #1e1e2d;
+                                font-weight: 500;
+                                margin: 0;
+                                font-size: 32px;
+                                font-family: 'Rubik', sans-serif;
+                              "
+                            >
+                              Ya casi terminamos...
+                            </h1>
+                            <p
+                              style="
+                                font-size: 15px;
+                                color: #455056;
+                                margin: 8px 0 0;
+                                line-height: 24px;
+                              "
+                            >
+                            ${JSON.stringify(receipt)}
+                            </p>
+      
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="height: 40px">&nbsp;</td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style="height: 20px">&nbsp;</td>
+                  </tr>
+                  <tr>
+                    <td style="text-align: center">
+                      <p
+                        style="
+                          font-size: 14px;
+                          color: rgba(69, 80, 86, 0.7411764705882353);
+                          line-height: 18px;
+                          margin: 0 0 0;
+                        "
+                      >
+                        &copy; <strong>Henry's Burger</strong>
+                      </p>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style="height: 80px">&nbsp;</td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+          <!--/100% body table-->
+        </body>
+      </html>`,
+    });
+
     res.status(201).json(order);
   } catch (error) {
     next(error);
